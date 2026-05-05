7ce073d46f05bcf4ef10ce1c72185c38f97e6dce
dmjgr5 <88672316+dmjgr5@users.noreply.github.com>
2026-05-05 16:47:26 +0900

    Update study.java

--- study.java
+++ study.java
@@ -62,6 +62,7 @@ public class Test {
 
 	static int[][] arr = { {}, { 2, 3, 8 }, { 1, 6, 8 }, { 1, 5 }, { 5, 7 }, { 3, 4, 7 }, { 2 }, { 4, 5 }, { 1, 2 } };
 	static Stack<Integer> stack = new Stack<Integer>();
+	//Stack과 다르게 전용 구현체가 존재하지 않는다. LinkedList를 구현체로 사용할 수 있다.
 	static Queue<Integer> queue = new LinkedList<Integer>();
 	static boolean[] visited = new boolean[9];
 
